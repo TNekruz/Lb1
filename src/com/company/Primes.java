7873411f1995dc73c0545dce6e15039ae9ad8bae
TNekruz <90762969+TNekruz@users.noreply.github.com>
2021-09-15 17:35:25 +0300

    lb1.1

---
+++ src/com/company/Primes.java
@@ -0,0 +1,25 @@
+package com.company;
+
+//вывод всех простых чисел от 0 до 100
+public class Primes {
+
+    //определяет простое число или нет
+    public static boolean isPrime(int n)
+    {
+        for (int i = 2; i<n; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        //проверяем все числа от 0 до 100 и если он простое, то выводим
+        for (int i = 2; i <=100; i++)
+        {
+            if (isPrime(i))
+                System.out.println(i);
+        }
+    }
+}
